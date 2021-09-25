3ef9e001cc57320424fe03e4f454d4c53b5e9072
Jadfii <Jadfii@users.noreply.github.com>
2021-09-25 22:24:19 +0100

    Add logo

--- src/components/Masthead/Masthead.tsx
+++ src/components/Masthead/Masthead.tsx
@@ -2,20 +2,23 @@ import { Button, Spacer } from '@geist-ui/react';
 import { RefreshCw } from '@geist-ui/react-icons';
 import React, { useContext } from 'react';
 import { ServersContext } from '../../contexts/ServersProvider';
+import Logo from '../Logo/Logo';
 import ServersSearch from '../ServersSearch/SeversSearch';
 
 const Masthead: React.FC = () => {
   const { refreshServers, isLoadingServers } = useContext(ServersContext);
 
   return (
-    <div className="flex items-center justify-center py-4">
-      <div style={{ width: '40%' }}>
+    <div className="flex items-center py-4">
+      <Logo />
+
+      <div className="ml-6" style={{ width: '40%' }}>
         <ServersSearch />
       </div>
 
       <Spacer w={1} />
 
-      <Button onClick={() => refreshServers()} icon={<RefreshCw />} loading={isLoadingServers}>
+      <Button onClick={() => refreshServers()} icon={<RefreshCw />} loading={isLoadingServers} marginLeft="auto">
         Refresh servers
       </Button>
     </div>
