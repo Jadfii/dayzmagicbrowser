6b30076388e3b0c0a97529fe5a13214447bac791
Jadfii <Jadfii@users.noreply.github.com>
2021-09-25 23:45:12 +0100

    Add custom search icon to servers search

--- src/components/ServersSearch/SeversSearch.tsx
+++ src/components/ServersSearch/SeversSearch.tsx
@@ -1,10 +1,13 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { AutoComplete } from '@geist-ui/react';
 import Fuse from 'fuse.js';
 import { ServersContext } from '../../contexts/ServersProvider';
 import ServerOption from './ServerOption';
 import useDebounce from '../../hooks/useDebounce';
+import { Search } from '@geist-ui/react-icons';
 
+const ELEMENT_ID = 'servers-search';
 const RESULTS_LIMIT = 100;
 
 const ServersSearch: React.FC = () => {
@@ -15,6 +18,8 @@ const ServersSearch: React.FC = () => {
   const debouncedSearchValue = useDebounce(searchValue, 500);
 
   const [isSearching, setIsSearching] = useState<boolean>(false);
+
+  const [hasInsertedIcon, setHasInsertedIcon] = useState<boolean>(false);
 
   function searchHandler(value: string) {
     setSearchValue(value);
@@ -51,6 +56,35 @@ const ServersSearch: React.FC = () => {
     setIsSearching(false);
   }, [debouncedSearchValue]);
 
+  // Inject the custom icon
+  // Workaround since AutoComplete doesn't accept an icon ??
+  useEffect(() => {
+    // Icon is already injected. Do nothing
+    if (hasInsertedIcon) return;
+
+    const inputElement = document.getElementById(ELEMENT_ID);
+    if (!inputElement) return;
+
+    inputElement.classList.add('left-icon');
+    inputElement.classList.remove('right-icon');
+
+    const renderElement = inputElement.parentElement;
+    if (!renderElement) return;
+
+    const iconElement = document.createElement('span');
+
+    const rightIconElement = document.querySelector('.input-icon');
+    if (!rightIconElement) return;
+
+    iconElement.className = rightIconElement.className;
+    renderElement.prepend(iconElement);
+
+    ReactDOM.render(<Search />, iconElement);
+
+    rightIconElement.remove();
+    setHasInsertedIcon(true);
+  }, []);
+
   return (
     <>
       <AutoComplete
@@ -62,6 +96,7 @@ const ServersSearch: React.FC = () => {
         scale={4 / 3}
         width="100%"
         searching={isSearching}
+        id={ELEMENT_ID}
       />
     </>
   );
