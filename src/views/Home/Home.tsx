9b1fa56a41d81853c2d211f704558bce0db863c8
Jadfii <Jadfii@users.noreply.github.com>
2021-09-26 00:15:30 +0100

    Add Home copy and styling

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,10 +8,18 @@ const Home: React.FC = () => {
     <div className="relative flex items-center flex-1">
       <BackgroundImage src="https://dayz-magic-launcher.s3.eu-west-2.amazonaws.com/images/home.jpg" />
 
-      <Grid.Container gap={2} justify="center" className="my-auto">
-        <Grid xs={24} className="flex-col">
-          <Text h1>Hello, World</Text>
-          <Text p>DayZ Magic Browser</Text>
+      <Grid.Container gap={2} className="my-auto">
+        <Grid xs={2}></Grid>
+
+        <Grid xs={10} className="flex-col">
+          <Text h1 margin={'0 0 1rem 0'} style={{ lineHeight: '1.4' }}>
+            Find your next favourite DayZ server
+          </Text>
+          <Text p margin={0}>
+            Browse & refine a full list of DayZ servers and connect directly into the game.
+            <br />
+            All from your browser.
+          </Text>
         </Grid>
       </Grid.Container>
     </div>
