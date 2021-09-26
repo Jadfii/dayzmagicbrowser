89b52f01941681d7cc2d9abacb62b343df5fd42b
Jadfii <Jadfii@users.noreply.github.com>
2021-09-26 01:11:08 +0100

    Add CTA to home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './home.scss';
-import { Grid, Text } from '@geist-ui/react';
+import { Button, Grid, Text } from '@geist-ui/react';
 import BackgroundImage from '../../components/BackgroundImage/BackgroundImage';
+import { ArrowRight } from '@geist-ui/react-icons';
+import { useHistory } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const history = useHistory();
+
   return (
     <div className="relative flex items-center flex-1">
       <BackgroundImage src="https://dayz-magic-launcher.s3.eu-west-2.amazonaws.com/images/home.jpg" />
@@ -11,15 +15,22 @@ const Home: React.FC = () => {
       <Grid.Container gap={2} className="my-auto">
         <Grid xs={2}></Grid>
 
-        <Grid xs={10} className="flex-col">
+        <Grid xs={10} className="relative flex-col">
           <Text h1 margin={'0 0 1rem 0'} style={{ lineHeight: '1.4' }}>
             Find your next favourite DayZ server
           </Text>
+
           <Text p margin={0}>
             Browse & refine a full list of DayZ servers and connect directly into the game.
             <br />
             All from your browser.
           </Text>
+
+          <div className="mt-6" style={{ width: '75%' }}>
+            <Button onClick={() => history.push('/servers')} type="success-light" iconRight={<ArrowRight />} scale={5 / 3} width="100%">
+              Browse servers
+            </Button>
+          </div>
         </Grid>
       </Grid.Container>
     </div>
