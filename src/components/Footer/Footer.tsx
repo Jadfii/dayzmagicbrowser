2cc3af7bb44dc472cba6d144a2f59638777c6c76
Jadfii <Jadfii@users.noreply.github.com>
2021-09-26 01:41:57 +0100

    Add footer content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,35 @@
-import { useTheme } from '@geist-ui/react';
+import { Link, Spacer, Text, useTheme } from '@geist-ui/react';
 import React from 'react';
+import Logo from '../Logo/Logo';
 
 const Footer: React.FC = () => {
   const theme = useTheme();
 
   return (
-    <div className="flex items-center w-full py-6 mt-auto" style={{ background: theme.palette.background }}>
-      <h2>Footer</h2>
+    <div className="flex w-full pt-12 py-24 mt-auto" style={{ background: theme.palette.background }}>
+      <div>
+        <Logo />
+
+        <Spacer h={1 / 2} />
+
+        <div className="max-w-sm">
+          <Text small type="secondary">
+            MagicLauncher is in no way affiliated with DayZ or Bohemia Interactive a.s.
+          </Text>
+
+          <Spacer h={1} />
+
+          <Text small type="secondary">
+            &copy; {new Date().getFullYear()} MagicLauncher. All rights reserved.
+          </Text>
+        </div>
+      </div>
+
+      <div className="ml-auto">
+        <Text small>
+          Made by <Link href="//github.com/Jadfii">Jad</Link>
+        </Text>
+      </div>
     </div>
   );
 };
