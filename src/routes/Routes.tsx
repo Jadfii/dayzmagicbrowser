8995f095e1bae05e2958a86a9c600b0123f3f33f
Jadfii <Jadfii@users.noreply.github.com>
2021-09-26 01:18:29 +0100

    Add servers route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,14 @@ import React, { Suspense } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 const Home = React.lazy(() => import('../views/Home/Home'));
+const Servers = React.lazy(() => import('../views/Servers/Servers'));
 
 const Routes: React.FC = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/servers" exact component={Servers} />
 
         <Route component={() => <Redirect to="/" />} />
       </Switch>
---
+++ src/views/Servers/Servers.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Servers: React.FC = () => {
+  return <div className="relative flex items-center flex-1"></div>;
+};
+
+export default Servers;
