f93c05f57161dbad061ec052f2e33d2bca297b90
Jadfii <Jadfii@users.noreply.github.com>
2021-10-02 21:34:03 +0100

    Fix eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), eslintPlugin()],
   server: {
     port: 3030,
     strictPort: true,
