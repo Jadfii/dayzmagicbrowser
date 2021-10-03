addf60032d2196578d62c0a8490172979ba90931
Jadfii <Jadfii@users.noreply.github.com>
2021-10-03 16:44:01 +0100

    Add title to Servers page

--- src/views/Servers/Servers.tsx
+++ src/views/Servers/Servers.tsx
@@ -1,9 +1,11 @@
+import { Text } from '@geist-ui/react';
 import React from 'react';
 import ServerList from '../../components/ServerList/ServerList';
 
 const Servers: React.FC = () => {
   return (
-    <div className="relative flex items-center flex-1 py-10">
+    <div className="relative flex flex-col flex-1 py-10">
+      <Text h2>Servers</Text>
       <ServerList />
     </div>
   );
