7f9f43dab26443ee298e156003db2a2e7d51f666
Jadfii <Jadfii@users.noreply.github.com>
2021-10-06 21:01:34 +0100

    Add arrow to mod card

--- src/components/ServerModList/ModCard.tsx
+++ src/components/ServerModList/ModCard.tsx
@@ -1,4 +1,5 @@
 import { Card, Text } from '@geist-ui/react';
+import { ArrowRight } from '@geist-ui/react-icons';
 import React from 'react';
 import { STEAM_WORKSHOP_ITEM } from '../../constants/links.constant';
 import { WorkshopMod } from '../../types/Types';
@@ -10,12 +11,18 @@ interface Props {
 const ModCard: React.FC<Props> = ({ mod }) => {
   return (
     <Card hoverable onClick={() => window.open(`${STEAM_WORKSHOP_ITEM}${mod.id}`, '_blank')} className="cursor-pointer">
-      {mod.name && (
-        <Text h6 margin={0}>
-          {mod.name}
-        </Text>
-      )}
-      {mod.subscriptions && <Text small>{mod.subscriptions.toLocaleString()} subscribers</Text>}
+      <Card.Content className="flex items-center">
+        <div>
+          {mod.name && (
+            <Text h6 margin={0}>
+              {mod.name}
+            </Text>
+          )}
+          {mod.subscriptions && <Text small>{mod.subscriptions.toLocaleString()} subscribers</Text>}
+        </div>
+
+        <ArrowRight className="ml-auto" />
+      </Card.Content>
     </Card>
   );
 };
