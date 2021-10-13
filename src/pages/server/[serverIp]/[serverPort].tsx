8f35820291aa77ad74428c07d515cd466da56291
Jadfii <Jadfii@users.noreply.github.com>
2021-10-14 00:12:19 +0100

    Fix getStaticPaths

--- src/pages/server/[serverIp]/[serverPort].tsx
+++ src/pages/server/[serverIp]/[serverPort].tsx
@@ -5,10 +5,11 @@ import { ServerObjectResponse } from 'types/ResponseTypes';
 export default ServerPage;
 
 export const getStaticPaths = async () => {
-  const servers: ServerObjectResponse[] = await get('servers', { limit: 10000, sortBy: 'players:desc' });
+  const res = await get('servers', { limit: 10000, sortBy: 'players:desc' });
+  const servers: ServerObjectResponse[] = res?.results || [];
 
   return {
-    paths: servers.map((server: ServerObjectResponse) => ({ params: { serverIp: server?.ip, serverPort: server?.query_port } })),
+    paths: servers?.map((server: ServerObjectResponse) => ({ params: { serverIp: server?.ip, serverPort: server?.query_port } })),
     fallback: true,
   };
 };
