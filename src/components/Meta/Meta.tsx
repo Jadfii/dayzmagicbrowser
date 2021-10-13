68e6e4a03bfdb66497b73c1d1f031844c7985822
Jadfii <Jadfii@users.noreply.github.com>
2021-10-13 23:13:54 +0100

    Add more meta tags

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,19 +1,34 @@
 import React, { useMemo } from 'react';
 import Head from 'next/head';
-import { TITLE_PREFIX } from '../../constants/meta.constant';
+import { DOMAIN, META_DESCRIPTION, TITLE_PREFIX } from '../../constants/meta.constant';
+import { IMAGE_BUCKET } from 'constants/links.constant';
+import { useRouter } from 'next/router';
 
 interface Props {
   title?: string;
 }
 
 const Meta: React.FC<Props> = ({ title }) => {
+  const router = useRouter();
   const metaTitle = useMemo(() => `${TITLE_PREFIX}${title ? `- ${title}` : ''}`, [title]);
 
   return (
     <>
       <Head>
         <title>{metaTitle}</title>
-        <meta property="og:title" content={metaTitle} />
+        <meta property="og:title" content={metaTitle} key="ogtitle" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        <meta name="description" content={META_DESCRIPTION} />
+        <meta property="og:description" content={META_DESCRIPTION} key="ogdesc" />
+
+        <meta property="og:site_name" content={TITLE_PREFIX} key="ogsitename" />
+
+        <meta property="og:image" content={`${IMAGE_BUCKET}home.jpg`} key="ogimage" />
+
+        <meta property="og:url" content={`${DOMAIN}${router?.pathname}`} key="ogurl" />
 
         <link rel="icon" href="/favicon.png" />
       </Head>
--- src/constants/meta.constant.ts
+++ src/constants/meta.constant.ts
@@ -1,1 +1,4 @@
 export const TITLE_PREFIX = 'DayZMagicLauncher';
+export const META_DESCRIPTION =
+  'Browse a full list of DayZ servers and connect directly into the game - all from your browser. No downloads required.';
+export const DOMAIN = 'https://browser.dayzmagiclauncher.com';
