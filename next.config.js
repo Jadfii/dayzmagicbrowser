8258f284365b0806f0ddc833c2b29d65b965ce7c
Jadfii <Jadfii@users.noreply.github.com>
2021-10-13 23:45:15 +0100

    Add trailing slashes to fix routing

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const securityHeaders = [
 
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     loader: 'custom',
     domains: ['dayz-magic-launcher.s3.eu-west-2.amazonaws.com'],
