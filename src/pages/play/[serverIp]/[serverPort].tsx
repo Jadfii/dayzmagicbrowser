eb4b74e0cdfffef45b75efad3ae412979ef1a2cf
Jadfii <Jadfii@users.noreply.github.com>
2021-10-14 21:24:53 +0100

    Remove static route pre-generation

--- src/pages/play/[serverIp]/[serverPort].tsx
+++ src/pages/play/[serverIp]/[serverPort].tsx
@@ -1,21 +1,3 @@
 import PlayServer from '../../../views/PlayServer/PlayServer';
-import { get } from 'services/HTTP';
-import { ServerObjectResponse } from 'types/ResponseTypes';
 
 export default PlayServer;
-
-export const getStaticPaths = async () => {
-  const res = await get('servers', { limit: 10000, sortBy: 'players:desc' });
-  const servers: ServerObjectResponse[] = res?.results || [];
-
-  return {
-    paths: servers?.map((server: ServerObjectResponse) => ({ params: { serverIp: `${server?.ip}`, serverPort: `${server?.query_port}` } })),
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async () => {
-  return {
-    props: { serverIp: '1', serverPort: '2' },
-  };
-};
