8be009da0eb095909a82bca75df8ae3863ab5247
Jadfii <Jadfii@users.noreply.github.com>
2021-10-16 00:26:46 +0100

    Make server list more responsive

--- src/components/ServerList/ServerList.tsx
+++ src/components/ServerList/ServerList.tsx
@@ -6,7 +6,7 @@ const ServerList: React.FC = () => {
   const { servers } = useContext(ServersContext);
 
   return (
-    <div className="grid grid-cols-4 grid-flow-row gap-6">
+    <div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-row gap-6">
       {servers.slice(0, 200).map((server, i) => (
         <ServerCard server={server} imageHeight={100} key={i} />
       ))}
