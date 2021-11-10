376c4103ffbe4e7f30edbe367301a5933812549d
Jadfii <Jadfii@users.noreply.github.com>
2021-11-10 10:13:37 +0000

    Use all servers endpoint to reduce servers payload size for static regeneration

--- src/pages/servers.tsx
+++ src/pages/servers.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from 'next';
-import { ServerResponse } from '../types/ResponseTypes';
+import { ServerObjectResponse } from '../types/ResponseTypes';
 import { mapServerResponse } from '../data/Mapper';
 import { get } from '../services/HTTP';
 
@@ -8,11 +8,11 @@ import Servers from '../views/Servers/Servers';
 export default Servers;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data: ServerResponse = await get(`servers`, { limit: 10000, sortBy: 'players:desc' });
+  const data: ServerObjectResponse[] = await get(`servers/all`, { exclude: ['mods', 'createdAt', 'updatedAt'].join(',') });
 
   return {
     props: {
-      servers: (data?.results?.map((server) => mapServerResponse(server)) || []).sort((a, b) => b.players + b.queue - (a.players + a.queue)),
+      servers: (data?.map((server) => mapServerResponse(server)) || []).sort((a, b) => b.players + b.queue - (a.players + a.queue)),
     },
     revalidate: 60, // Fetch new data every 60 seconds
   };
