57f164db9b8836b6da3d06f7930313a4eee81071
Jadfii <Jadfii@users.noreply.github.com>
2022-04-17 04:19:02 +0100

    Add empty state to servers page

--- src/components/ServerList/ServerList.tsx
+++ src/components/ServerList/ServerList.tsx
@@ -1,3 +1,5 @@
+import { Text } from '@geist-ui/react';
+import { Frown } from '@geist-ui/react-icons';
 import React from 'react';
 import { Server } from '../../types/Types';
 import ServerCard from '../ServerCard/ServerCard';
@@ -10,6 +12,19 @@ interface Props {
 }
 
 const ServerList: React.FC<Props> = ({ servers, isLoading }) => {
+  if (servers.length === 0)
+    return (
+      <div className="flex flex-col items-center justify-center mt-24">
+        <Frown size={48} />
+        <Text h2 my={0}>
+          No servers found
+        </Text>
+        <Text p my={0}>
+          Try adjusting your filter options to find servers.
+        </Text>
+      </div>
+    );
+
   return (
     <div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-row gap-6">
       {!isLoading
