e014e53efc80f089a594971f307b3df3b6d87e51
Jadfii <Jadfii@users.noreply.github.com>
2022-12-15 23:21:31 +0000

    Update packages

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -4,14 +4,14 @@ import Masthead from '../Masthead/Masthead';
 import Footer from '../Footer/Footer';
 import { Inter } from '@next/font/google';
 
-const inter = Inter();
+const font = Inter();
 
 const AppLayout: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
   const theme = useTheme();
 
   return (
     <>
-      <div className={`w-screen h-full ${inter.className}`}>
+      <div className={`w-screen h-full ${font.className}`}>
         <div className="container relative flex flex-col w-full h-full min-h-screen" style={{ background: theme.palette.accents_1 }}>
           <div className="relative flex flex-col flex-auto min-h-screen">
             <Masthead />
