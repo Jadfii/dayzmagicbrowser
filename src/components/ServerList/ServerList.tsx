63ce8ef9b4e59fac09984809cafffd8c1c9e02ed
Jadfii <Jadfii@users.noreply.github.com>
2023-03-06 01:04:32 +0000

    eh

--- src/components/ServerList/ServerList.tsx
+++ src/components/ServerList/ServerList.tsx
@@ -11,18 +11,14 @@ interface Props {
 }
 
 const ServerList: React.FC<Props> = ({ servers, isLoading = false, onResetFilters }) => {
-  return (
-    <>
-      {!isLoading && servers.length === 0 && <ServersEmptyState onResetFilters={onResetFilters} />}
+  if (!isLoading && servers.length === 0) return <ServersEmptyState onResetFilters={onResetFilters} />;
 
-      {servers.length > 0 && (
-        <div className="grid grid-flow-row grid-cols-1 gap-6 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-          {!isLoading
-            ? servers.map((server, i) => <ServerCard server={server} imageHeight={100} key={i} />)
-            : [...Array(SERVERS_PAGE_SERVERS_COUNT).keys()].map((_, i) => <ServerCard imageHeight={100} isLoading key={i} />)}
-        </div>
-      )}
-    </>
+  return (
+    <div className="grid grid-flow-row grid-cols-1 gap-6 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      {!isLoading
+        ? servers.map((server, i) => <ServerCard server={server} imageHeight={100} key={i} />)
+        : [...Array(SERVERS_PAGE_SERVERS_COUNT).keys()].map((_, i) => <ServerCard imageHeight={100} isLoading key={i} />)}
+    </div>
   );
 };
 
